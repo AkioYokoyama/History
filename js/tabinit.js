48554b40181141092b96826451fa954fcb8aafb4
mk3250d07295y@aol.com <Yoika>
2022-05-07 20:08:38 +0900

    delete 古いファイルの削除

--- js/tabinit.js
+++
@@ -1,8 +0,0 @@
-tab.setup = {
-  tabs: document.getElementById('tab').getElementsByTagName('li'),
-  pages: [
-    document.getElementById('historyTop10_div'),
-    document.getElementById('bookmarks_div'),
-  ]
-} //オブジェクトをセット
-tab.init(); //起動！
