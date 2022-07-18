5c23220a945e6a9ac8b0d2c756af7b68e3ae6c44
mk3250d07295y@aol.com <Yoika>
2022-07-18 19:43:12 +0900

    update 不要なコメントを削除

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -97,7 +97,6 @@ const Popup: FC = () => {
 }
 
 const rootElement = document.getElementById('root');
-// https://blog.logrocket.com/how-to-use-typescript-with-react-18-alpha/
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 root.render(<Popup />);
