1181c49076a287445266d7671525ecf97b6f40eb
akio.yokoyama.yoika@gmail.com <Yoika>
2026-09-06 12:44:50 +0900

    ロジックとレンダリングを分離

--- src/options/components/FiltersInput.tsx
+++ src/options/components/FiltersInput.tsx
@@ -1,34 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useFiltersWhitelist } from '../modules/useFiltersWhitelist';
 
 export function FiltersInput() {
-  const storageHistoryWhitelist: string = localStorage.getItem('historyFilters') ?? JSON.stringify([]);
-
-  const [whitelist, setWhitelist] = useState(JSON.parse(storageHistoryWhitelist));
-  const [historyWhitelist, setHistoryWhitelist] = useState('');
-  const [formValue, setFormValue] = useState('');
-
-  const handleHistoryWhitelistChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setHistoryWhitelist(e.target.value);
-  }
-
-  useEffect(() => { }, [whitelist, formValue]);
-
-  const handleAddButtonClick = (): void => {
-    if (!historyWhitelist) return;
-
-    const currnetStorageWhitelist: string = localStorage.getItem('historyFilters') ?? JSON.stringify([]);
-    const newStorageWhitelist = JSON.parse(currnetStorageWhitelist);
-    newStorageWhitelist.push(historyWhitelist)
-    localStorage.setItem('historyFilters', JSON.stringify(newStorageWhitelist));
-    setWhitelist(newStorageWhitelist);
-    setFormValue('');
-  }
-
-  const handleDeleteIconClick = (e: React.MouseEvent<HTMLImageElement>) => {
-    const newWhitelist = whitelist.filter((f: string) => f !== e.currentTarget.dataset.url);
-    localStorage.setItem('historyFilters', JSON.stringify(newWhitelist));
-    setWhitelist(newWhitelist);
-  }
+  const {
+    whitelist,
+    inputValue,
+    handleInputChange,
+    addToWhitelist,
+    removeFromWhitelist,
+  } = useFiltersWhitelist();
 
   return (
     <div className="mt-5">
@@ -38,8 +17,8 @@ export function FiltersInput() {
           id="whitelist"
           type="text"
           className="px-2 py-1 border border-gray-300 rounded-md shadow-sm text-sm text-gray-900 focus:outline-none"
-          defaultValue={formValue}
-          onChange={handleHistoryWhitelistChange}
+          defaultValue={inputValue}
+          onChange={handleInputChange}
         />
       </div>
       <div className="text-center">
@@ -47,7 +26,7 @@ export function FiltersInput() {
           className="w-16 py-1.5 text-white border-none rounded bg-violet-500 hover:opacity-70 hover:cursor-pointer"
           type="button"
           value="追加"
-          onClick={handleAddButtonClick}
+          onClick={addToWhitelist}
         />
       </div>
 
@@ -58,7 +37,7 @@ export function FiltersInput() {
             <li>
               <span className="flex items-center leading-5 hover:underline">
                 <img
-                  onClick={handleDeleteIconClick}
+                  onClick={() => removeFromWhitelist(whitelistUrl)}
                   data-url={whitelistUrl}
                   className="w-2.5 h-2.5 mr-1.5 hover:cursor-pointer"
                   src={chrome.runtime.getURL("img/cross16.svg")}
