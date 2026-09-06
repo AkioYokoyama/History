4127dfccd03b0da44af94190d6d01a340f9f909d
akio.yokoyama.yoika@gmail.com <Yoika>
2026-09-06 11:30:50 +0900

    moduleの分離

---
+++ src/popup/modules/usePopupProps.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect, MouseEvent } from 'react';
+import { HistoryType, DefaultStartTime } from '../types/HistoryType';
+import { getFilters } from './getFilters';
+import { sortHistories } from './sortHistories';
+import { filterHistories } from './filterHistories';
+
+export function usePopupProps() {
+  const [histories, setHistories] = useState<HistoryType[]>([]);
+  const [historyTerm, ] = useState(localStorage['historyTerm'] ?? localStorage['historyTerm']);
+  const [isInitialize, setIsInitialize] = useState(false);
+  const deleteHistory = (url: string) => chrome.history.deleteUrl({ url: url });
+
+  const handleClickDelete = (e: MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    if (!e.currentTarget.dataset.url) return;
+    deleteHistory(e.currentTarget.dataset.url);
+    const updatedHistories = histories.filter((deleteHistory) => deleteHistory.url !== e.currentTarget.dataset.url);
+    setHistories(updatedHistories);
+  }
+
+  const handleClickAllDeleteButton = () => {
+    chrome.history.deleteAll();
+    setHistories([]);
+  }
+
+  useEffect(() => {
+    chrome.history.search({'text': '', 'startTime': DefaultStartTime}, (historyItems: any) => {
+      const sortedHistories = sortHistories(historyItems);
+      const filteredHistories = filterHistories(sortedHistories, getFilters, deleteHistory);
+      setHistories(filteredHistories);
+      setIsInitialize(true);
+    });
+  }, [isInitialize, historyTerm]);
+
+  return {
+    histories,
+    handleClickDelete,
+    handleClickAllDeleteButton,
+  }
+}
